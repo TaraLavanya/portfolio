e6a4bcde2df44b9da4f7840f635cd1772f4009d9
Lavanya <lavanyam@agilehealth.in>
2024-01-30 13:02:28 +0530

    rm: footer

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -2,47 +2,5 @@
 	<title>Portfolio</title>
 </head>
 <body>
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="full"></div>
-				</div>
-				<div class="col-md-8">
-					<div class="row">
-						<div class="col-md-7">
-							<div class="row">
-								<div class="col-md-6">
-									<div class="widget_menu">
-										<h3>Menu</h3>
-										<ul>
-											<li><a href="#">Home</a></li>
-											<li><a href="#">About</a></li>
-											<li><a href="#">Services</a></li>
-											<li><a href="#">Testimonial</a></li>
-											<li><a href="#">Blog</a></li>
-											<li><a href="#">Contact</a></li>
-										</ul>
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="widget_menu">
-										<h3>Account</h3>
-										<ul>
-											<li><a href="#">Account</a></li>
-											<li><a href="#">Checkout</a></li>
-											<li><a href="#">Login</a></li>
-											<li><a href="#">Register</a></li>
-											<li><a href="#">Shopping</a></li>
-											<li><a href="#">Widget</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
+
 </body>
